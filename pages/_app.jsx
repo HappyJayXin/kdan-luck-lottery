ef0231befd2114ad97353745c4957fa5943d95ad
Jay <happyjayxin@gmail.com>
2024-12-03 17:14:06 +0800

    feat: add beforeunload confirmation prompt for page unload actions

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import store from '../store'
-import { Provider } from 'react-redux'
+import store from '../store';
+import { Provider } from 'react-redux';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -9,15 +9,28 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = '';
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <>
       <GlobalStyle />
@@ -27,5 +40,5 @@ export default function App({ Component, pageProps }) {
         </Provider>
       </ThemeProvider>
     </>
-  )
+  );
 }
