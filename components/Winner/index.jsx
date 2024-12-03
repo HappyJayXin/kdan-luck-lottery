26812ea42834d05e312f0fd551e93aae3e723a79
Jay <happyjayxin@gmail.com>
2024-12-03 18:11:42 +0800

    enhancement: winner animate

--- components/Winner/index.jsx
+++ components/Winner/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -11,17 +11,18 @@ const Mask = styled.div`
   width: 100%;
   height: 100vh;
   flex-direction: column;
-  justify-content: center; 
+  justify-content: center;
   align-items: center;
   opacity: 0;
   display: none;
   transition: opacity 5s 5s linear;
 `;
+
 const WinnerContainer = styled.div`
   min-width: 600px;
   min-height: 150px;
   max-height: 500px;
-  background-color: #FEFFF9;
+  background-color: #fefff9;
   margin-bottom: 50px;
   color: #000000;
   font-family: 'Noto Sans TC', sans-serif;
@@ -30,7 +31,7 @@ const WinnerContainer = styled.div`
   box-shadow: 1px 4px 5px rgba(76, 87, 96, 0.3);
   border: #000000 2px solid;
   border-radius: 8px;
-  overflow: scroll;
+  overflow-y: auto;
   padding: 24px;
   box-sizing: border-box;
 
@@ -41,6 +42,14 @@ const WinnerContainer = styled.div`
   li {
     margin: 0;
     padding: 0;
+    opacity: 0;
+    transform: translateY(20px);
+    transition: opacity 0.6s ease-in, transform 0.6s ease-in;
+  }
+
+  .show {
+    opacity: 1;
+    transform: translateY(0);
   }
 `;
 
@@ -68,27 +77,90 @@ const Winner = () => {
   const winnerList = useSelector((state) => state.main.winnerList);
   const dispatch = useDispatch();
 
-  const handleClick = () => {
-    dispatch(setOpened(false));
-    dispatch(setActive(false));
+  const [visibleWinners, setVisibleWinners] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const winnerListRef = useRef(null);
+  const winnerRefs = useRef([]);
+
+  if (winnerRefs.current.length !== winnerList.length) {
+    winnerRefs.current = Array(winnerList.length)
+      .fill()
+      .map((_, i) => winnerRefs.current[i] || React.createRef());
   }
+
+  useEffect(() => {
+    if (isOpened && winnerList.length > 0) {
+      setVisibleWinners(0);
+      setIsAnimating(true);
+      let currentIndex = 0;
+
+      const animationInterval = setInterval(() => {
+        currentIndex += 1;
+        setVisibleWinners(currentIndex);
+
+        if (winnerListRef.current) {
+          winnerListRef.current.scrollTo({
+            top: winnerListRef.current.scrollHeight,
+            behavior: 'smooth',
+          });
+        }
+
+        if (currentIndex >= winnerList.length) {
+          clearInterval(animationInterval);
+          setIsAnimating(false);
+        }
+      }, 500);
+
+      return () => {
+        clearInterval(animationInterval)
+      };
+    }
+  }, [isOpened, winnerList]);
+
+  useEffect(() => {
+    if (visibleWinners > 0 && winnerRefs.current[visibleWinners - 1]) {
+      winnerRefs.current[visibleWinners - 1].current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    }
+  }, [visibleWinners]);
+
+  const handleClick = () => {
+    if (!isAnimating) {
+      setVisibleWinners(0);
+      dispatch(setOpened(false));
+      dispatch(setActive(false));
+      winnerListRef.current.scrollTop = 0;
+    }
+  };
 
   return (
     <>
       <Mask style={isOpened ? { display: 'flex', opacity: 100 } : {}}>
         <Wrapper>
-          <CloseBtn onClick={handleClick}>
-            <i className="fas fa-times fa-2x"></i>
-          </CloseBtn>
-          <WinnerContainer>
+          {!isAnimating && (
+            <CloseBtn onClick={handleClick}>
+              <i className="fas fa-times fa-2x"></i>
+            </CloseBtn>
+          )}
+          <WinnerContainer ref={winnerListRef}>
             <ol>
-              {winnerList.map((ele, index) => <li key={`winner_${index}`}>{ele}</li>)}
+              {winnerList.map((ele, index) => (
+                <li
+                  key={`winner_${index}`}
+                  ref={winnerRefs.current[index]}
+                  className={index < visibleWinners ? 'show' : ''}
+                >
+                  {ele}
+                </li>
+              ))}
             </ol>
           </WinnerContainer>
         </Wrapper>
       </Mask>
     </>
-  )
-}
+  );
+};
 
 export default Winner;
